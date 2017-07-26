b2bf485d09022eb94336d4a3cd5a7d6986b77df0
Peerlogic Systems <kanhaiya@peerlogicsys.com>
2017-07-26 11:38:15 +0530

    Building issue resplved

---
+++ EndoBindingXamarin/ApiDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace EndoBindingXamarin
+{
+	[Static]
+	//[Verify(ConstantsInterfaceAssociation)]
+	partial interface Constants
+	{
+		// extern double EndoClientVersionNumber;
+		[Field("EndoClientVersionNumber", "__Internal")]
+		double EndoClientVersionNumber { get; }
+
+		// extern const unsigned char [] EndoClientVersionString;
+		[Field("EndoClientVersionString", "__Internal")]
+        IntPtr EndoClientVersionString { get; }
+	}
+
+
+
+
+}
